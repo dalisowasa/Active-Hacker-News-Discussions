4204e60dc6bd1a5353d9a2261bf64d27b0ef80bb
dalisowasa <wasadaliso@yahoo.com>
2024-02-19 10:00:47 -0600

    Added visualization for data

--- hn_submissions_exercise.py
+++ hn_submissions_exercise.py
@@ -2,6 +2,7 @@ from operator import itemgetter
 
 import requests
 
+import plotly.express as px
 
 # Make an API call and check the response.
 url = 'https://hacker-news.firebaseio.com/v0/topstories.json'
@@ -34,3 +35,29 @@ for submission_dict in submission_dicts:
     print(f"\nTitle: {submission_dict['title']}")
     print(f"Discussion link: {submission_dict['hn_link']}")
     print(f"Comments: {submission_dict['comments']}")
+
+
+article_link, comments, hover_texts = [], [], []
+for s_dict in submission_dicts:
+    a = s_dict["title"]
+    b = s_dict["comments"]
+    c = s_dict["hn_link"]
+    d = f"<a href='{c}'>{a}</a>"
+    article_link.append(d)
+    comments.append(b)
+    hover_texts.append(a)
+
+
+
+# Make visualization.
+title = "Most Active Discussions on Hacker News"
+labels = {'x': 'Article', 'y': 'Comments'}
+fig = px.bar(x=article_link, y=comments, title=title, 
+             labels=labels, hover_name=hover_texts)
+
+fig.update_layout(title_font_size=28, xaxis_title_font_size=20,
+        yaxis_title_font_size=20)
+
+fig.update_traces(marker_color='LightGoldenRodYellow', marker_opacity=0.5)
+
+fig.show()
